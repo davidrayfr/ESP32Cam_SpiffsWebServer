1bbec271b67e7634c1093bdbe444a1d6623f9512
davidrayfr <davidrayfr@gmail.com>
2022-10-13 10:18:38 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   .vscode/extensions.json
    	new file:   data/index.html
    	new file:   data/script.js
    	new file:   data/w3.css
    	new file:   include/README
    	new file:   lib/README
    	new file:   platformio.ini
    	new file:   src/main.cpp
    	new file:   test/README

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// Web Server with SPIFFS
+
+#include <Arduino.h>
+#include <spiffs.h>
+#include "ESPAsyncWebServer.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
